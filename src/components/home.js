81efad504f57e5e04ad5fc65a93b06bac676236f
Aishwarya <aishwaryabaral25@gmail.com>
2025-12-28 19:30:03 +0530

    second commit

---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const home = () => {
+  return (
+    <div className = "home-container">
+        <navbar><navbar>
+    </div>
+  )
+}
+
+export default home
---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FaShoppingCart } from "react-icons/fa";
+
+
+export const navbar = () => {
+  return (
+    <nav>
+        <div className = "nav-logo">
+            <img src = "/images/logo.png" alt = "logo"></img>
+        </div>
+    </nav>
+  )
+}
